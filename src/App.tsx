38b8234af9fa029d72b7f0d75a3aaa789ac721be
Leandro Quadros <leandro.quadros@gmail.com>
2025-10-20 13:38:44 -0300

    input text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Button from "./components/button";
+import TextInput from "./components/text-input";
 
 function App() {
   return (
-    <div className="flex gap-3 px-2 py-2">
-      <Button>AGENDAR</Button>
-      <Button disabled>AGENDAR</Button>
+    <div className="fex flex-col space-y-5">
+      <div className="flex gap-3 px-2 py-2">
+        <Button>AGENDAR</Button>
+        <Button disabled>AGENDAR</Button>
+      </div>
+      <div className="flex gap-3 px-2 py-2">
+        <TextInput placeholder="Nome do cliente" />
+      </div>
     </div>
   );
 }
--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -1,1 +1,47 @@
-export default function TextInput() {}
+import { cva, cx, type VariantProps } from "class-variance-authority";
+import UserIcon from "../assets/icons/user-retangle.svg?react";
+import Icon from "./icon";
+
+const inputTextVariants = cva(
+  "border-1 border-solid border-gray-400 rounded-lg px-9 py-2 focus:outline-none focus:ring-1 focus:ring-yellow-dark",
+  {
+    variants: {
+      size: {
+        md: "h-12 pb-2 px-2",
+      },
+      disabled: {
+        true: "pointer-events-none",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+      disabled: false,
+    },
+  },
+);
+
+interface InputTextProps
+  extends VariantProps<typeof inputTextVariants>,
+    Omit<React.ComponentProps<"input">, "size" | "disabled"> {}
+
+export default function TextInput({
+  size,
+  disabled,
+  ...props
+}: InputTextProps) {
+  return (
+    <label className="relative text-gray-400 focus-within:text-gray-600 block">
+      <Icon
+        className="w-5 h-5 absolute top-1/2 transform -translate-y-1/2 left-2 text-yellow"
+        svg={UserIcon}
+      />
+      <input
+        className={cx(
+          inputTextVariants({ size, disabled }),
+          "placeholder-gray-400 text-gray-200",
+        )}
+        {...props}
+      />
+    </label>
+  );
+}
